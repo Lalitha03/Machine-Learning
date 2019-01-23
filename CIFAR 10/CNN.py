615dc953ad0b3876ad16102500d264f3d9163edc
albertlai431 <44041989+albertlai431@users.noreply.github.com>
2019-01-22 21:45:56 -0500

    Create CNN.py

---
+++ CIFAR 10/CNN.py
@@ -0,0 +1,9 @@
+import torch
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data.sampler import SubsetRandomSampler
+import matplotlib.pyplot as plt
+%matplotlib inline
+import torch.nn as nn
+import torch.nn.functional as F
